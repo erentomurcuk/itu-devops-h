e095ad01912bd24078c41fdfcd60f5c1b174b1b1
Herover <hi@leonora.app>
2022-02-09 14:35:00 +0100

    Add spark dep

--- minitwit/src/main/java/Main.java
+++ minitwit/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Minitwit");
+
+        port(8080);
+
+        staticFiles.location("/static");
+        get("/hello", (req, res) -> "Hello");
     }
 }
