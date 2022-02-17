47108adfb00f89b4ca788c429dc88a3cf87f1172
Herover <hi+git@leonora.app>
2022-02-17 23:01:59 +0100

    Add title tag values

--- minitwit/src/main/java/WebApplication.java
+++ minitwit/src/main/java/WebApplication.java
@@ -125,6 +125,10 @@ public class WebApplication {
 
             ctx.put("date", new DateTool());
 
+            if (!ctx.containsKey("title")) {
+                ctx.put("title", "Welcome");
+            }
+
             // "Run" template and return result
             StringWriter writer = new StringWriter();
             template.merge(ctx, writer);
@@ -274,8 +278,7 @@ public class WebApplication {
         // TODO: Port flask "flashes"
 
         model.put("splash", URLS.USER);
-        // Where does this come from in python?
-        model.put("title", "Public timeline");
+        model.put("title", loggedInUser.getString("username"));
 
         var db = new SQLite();
         var conn = db.getConnection();
@@ -317,8 +320,6 @@ public class WebApplication {
             // TODO: Port flask "flashes"
 
             model.put("splash", URLS.USER);
-            // Where does this come from in python?
-            model.put("title", "Public timeline");
 
             var db = new SQLite();
             var conn = db.getConnection();
@@ -338,6 +339,8 @@ public class WebApplication {
             profileUser.put("email", profileRs.getString("email"));
             model.put("profile_user", profileUser);
             profileRs.close();
+
+            model.put("title", profileUser.get("username"));
 
             var messageStmt = conn.prepareStatement(
                     "select message.*, user.* from message, user where\n" +
@@ -387,6 +390,7 @@ public class WebApplication {
             //model.put("splash", URLS.PUBLIC_TIMELINE);
             model.put("username", request.queryParams("username") == null ? "" : request.queryParams("username"));
             model.put("email", request.queryParams("email") == null ? "" : request.queryParams("email"));
+            model.put("title", "Sign Up");
 
             if (request.requestMethod().equals("POST")) {
                 if (request.queryParams("username") == null
@@ -434,6 +438,7 @@ public class WebApplication {
             response.redirect(URLS.USER);
         }
         Map<String, Object> model = new HashMap<>();
+        model.put("title", "Sign in");
 
         var enteredUserName = request.queryParams("username");
         var enteredPW = request.queryParams("password");
