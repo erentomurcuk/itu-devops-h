bdae7000c883d83f3a3230db3706041fa6d09c64
Herover <hi+git@leonora.app>
2022-03-03 20:25:08 +0100

    Repeat openjdk+maven install tries

--- terraform/files/install_jdk_maven.sh
+++ terraform/files/install_jdk_maven.sh
@@ -5,7 +5,8 @@
 # Must run as root.
 
 apt-get update
-apt-get install -y openjdk-17-jdk:amd64=17.0.1+12-1~18.04
-apt-get install -y maven:amd64=3.6.0-1~18.04.1
+until apt-get install -y \
+    openjdk-17-jdk:amd64=17.0.1+12-1~18.04 \
+    maven:amd64=3.6.0-1~18.04.1; do sleep 1; done
 
 export PATH=/opt/apache-maven-3.8.4/bin:$PATH
