07f3ac85f8f1307d1dd886c0b0aececf9771b069
Herover <hi+git@leonora.app>
2022-03-03 21:57:19 +0100

    Source .profile in build script, increase install delay

--- terraform/files/install_jdk_maven.sh
+++ terraform/files/install_jdk_maven.sh
@@ -8,8 +8,8 @@ echo "Updating..."
 apt-get update
 
 echo "Installing openjdk-17-jdk and openjdk-17-jre"
-until apt-get install -y "openjdk-17-jre"; do sleep 1; done
-until apt-get install -y "openjdk-17-jdk"; do sleep 1; done
+until apt-get install -y "openjdk-17-jre"; do sleep 10; done
+until apt-get install -y "openjdk-17-jdk"; do sleep 10; done
 
 echo "Downloading maven"
 # Note: default download server (dlcdn.apache.org) returns 503 to Digital Ocean IPs at the moment
@@ -20,5 +20,3 @@ tar zxvf /tmp/maven.tar.gz -C /opt
 echo "Adding maven bin path to PATH"
 export PATH=$PATH:/opt/apache-maven-3.8.4/bin
 echo "export PATH=$PATH:/opt/apache-maven-3.8.4/bin" > ~/.profile
-mvn --version
-ls /opt/apache-maven-3.8.4/bin
