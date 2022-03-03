2d4dfd58a322747b7403474f90c2e16358d0012c
Herover <hi+git@leonora.app>
2022-03-03 20:49:58 +0100

    Change maven install to newer version using manual install

--- terraform/files/install_jdk_maven.sh
+++ terraform/files/install_jdk_maven.sh
@@ -7,6 +7,8 @@
 apt-get update
 until apt-get install -y \
     "openjdk-17-jdk:amd64=17.*" \
-    "openjdk-17-jre:amd64=17.*" \
-    "maven:amd64=3.*"; do sleep 1; done
+    "openjdk-17-jre:amd64=17.*"; do sleep 1; done
 
+wget -O "/tmp/maven.tar.gz" https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+tar zxvf /tmp/maven.tar.gz -C /opt
+cp /opt/apache-maven-3.8.4/bin/* /usr/share/bin/
