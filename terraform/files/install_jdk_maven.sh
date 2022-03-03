5e6f8e4780ae212b3358a70bfc43cc75cff09f21
Herover <hi+git@leonora.app>
2022-03-03 21:41:08 +0100

    Add some debug messages to maven install

--- terraform/files/install_jdk_maven.sh
+++ terraform/files/install_jdk_maven.sh
@@ -11,9 +11,13 @@ echo "Installing openjdk-17-jdk and openjdk-17-jre"
 until apt-get install -y "openjdk-17-jre"; do sleep 1; done
 until apt-get install -y "openjdk-17-jdk"; do sleep 1; done
 
-echo "Installing "
+echo "Downloading maven"
 # Note: default download server (dlcdn.apache.org) returns 503 to Digital Ocean IPs at the moment
 # 2022-03-03
 wget -O "/tmp/maven.tar.gz" https://downloads.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+echo "Unpacking maven"
 tar zxvf /tmp/maven.tar.gz -C /opt
+echo "Adding maven bin path to PATH"
 export PATH=$PATH:/opt/apache-maven-3.8.4/bin
+mvn --version
+ls /opt/apache-maven-3.8.4/bin
