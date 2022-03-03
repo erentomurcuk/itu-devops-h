dfa237028e4b5e3bebb30180aeab7b2a1a571dbb
Herover <hi+git@leonora.app>
2022-03-03 20:19:15 +0100

    Fix openjdk+maven version identifiers

--- terraform/files/install_jdk_maven.sh
+++ terraform/files/install_jdk_maven.sh
@@ -5,7 +5,7 @@
 # Must run as root.
 
 apt-get update
-apt-get install -y openjdk-17-jdk:amd64=17.0.1+12-1~18.04.1
+apt-get install -y openjdk-17-jdk:amd64=17.0.1+12-1~18.04
 apt-get install -y maven:amd64=3.6.0-1~18.04.1
 
 export PATH=/opt/apache-maven-3.8.4/bin:$PATH
