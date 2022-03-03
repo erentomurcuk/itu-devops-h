6b4b2727fe5ea9c3c479948e3c7f66c8c66fc70a
Herover <hi+git@leonora.app>
2022-03-03 20:06:26 +0100

    Add retries to maven download
    Sometimes download page is down?

--- terraform/files/install_jdk_maven.sh
+++ terraform/files/install_jdk_maven.sh
@@ -4,10 +4,12 @@
 # Maven is only available after running this script, not from new terminals
 # Must run as root.
 
-apt update
-apt install -y openjdk-17-jdk:amd64=17.0.1+12-1~20.04
+apt-get update
+apt-get install -y openjdk-17-jdk:amd64=17.0.1+12-1~20.04
 
-wget -O "/tmp/maven.tar.gz" https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+let maxtries=10
+let tries=0
+until wget -O "/tmp/maven.tar.gz" https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz || [ "$tries" -eq $maxtries ]; do let a++; sleep 1; done
 tar zxvf /tmp/maven.tar.gz -C /opt
 ln -s /opt/apache-maven-3.8.4 /opt/maven
 
