8e15e0f63a9e90f6ac5cac56349f282bca077be2
Herover <hi+git@leonora.app>
2022-03-06 14:34:19 +0100

    Fix web setup script password argument

--- terraform/setup_server_web.sh
+++ terraform/setup_server_web.sh
@@ -4,7 +4,7 @@
 # First parameter is IP, next is NEW cleartext root password
 
 IP=$1
-ROOT_PASSWORD=`echo $1 | mkpasswd --stdin`
+ROOT_PASSWORD=`echo "$2" | mkpasswd --stdin`
 
 scp ./files/install_docker.sh root@$IP:/tmp/
 ssh root@$IP cd /tmp \&\& chmod +x ./install_docker.sh \&\& ./install_docker.sh
