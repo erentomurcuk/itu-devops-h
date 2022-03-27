66d0589eec350d3c7ebffaa5fac2f6de192fbc29
Herover <hi+git@leonora.app>
2022-03-27 22:58:37 +0200

    Forgot to add setup monitoring server script

--- terraform/setup_server_monitoring.sh
+++ terraform/setup_server_monitoring.sh
@@ -8,9 +8,15 @@ IP=$1
 scp ./files/install_docker.sh root@$IP:/tmp/
 ssh root@$IP cd /tmp \&\& chmod +x ./install_docker.sh \&\& ./install_docker.sh
 
+scp ./files/install_loki_docker_driver.sh root@$IP:/tmp/
+ssh root@$IP cd /tmp \&\& chmod +x ./install_loki_docker_driver.sh \&\& ./install_loki_docker_driver.sh
+
 ssh root@$IP mkdir -p /root/monitoring
 scp ./files/monitoring/docker-compose.yml root@$IP:/root/monitoring/docker-compose.yml
 scp ./files/monitoring/prometheus.yml root@$IP:/opt/prometheus.yml
+scp ./files/monitoring/loki-local-config.yml root@$IP:/opt/loki-local-config.yml
+scp ./files/promtail-config.yml root@$IP:/opt/promtail-config.yml
+
 # User in container has root group, so make sure root group in host can read+write volume mounts
 ssh root@$IP mkdir -p /opt/grafana /opt/prometheus
 ssh root@$IP chmod --recursive g+rw /opt/grafana
